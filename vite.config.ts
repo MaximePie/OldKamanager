1a7abffd3f2671e911f969b646bf2380739961d6
Maxime Pie <maxime.pie@mistertemp.com>
2024-04-11 15:49:10 +0200

    refactor: remove unused features

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import MillionLint from '@million/lint';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+var plugins = [react()];
+plugins.unshift(MillionLint.vite())
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: plugins
+});
